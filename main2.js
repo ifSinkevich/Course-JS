abd66bd748fb71ef72cfed46f07f389fef3dd8f4
Sinkevich Ilona <Sinkevich.Ilona@gmail.com>
2020-10-09 19:54:06 +0300

    Убрала скобки для переменных в typeof

--- main2.js
+++ main2.js
@@ -8,9 +8,9 @@ let budgetDay = money / 30;
 
 
 
-console.log(typeof(money));
-console.log(typeof(income));
-console.log(typeof(deposit));
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ', period, ' месяцев');
 console.log('Цель - заработать', mission, '$');
