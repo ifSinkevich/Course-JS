1322a15532e4a4c09e65b6f5ae2ed88cd1e15119
Sinkevich Ilona <Sinkevich.Ilona@gmail.com>
2020-10-10 08:16:00 +0300

    исправила структуру

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-let money, income, addExpenses, deposit, mission, period;
-alert("Hello world!");
-console.log("Start OK)");
+let money = 300;
+let income = 'фриланс';
+let addExpenses = 'коммуналка, маршрутка, интернет, парикмахерская, сладости';
+let deposit = true;
+let mission = 10000;
+let period = 12;
+let budgetDay = money / 30;
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ', period, ' месяцев');
+console.log('Цель - заработать', mission, '$');
+addExpenses.toLowerCase();
+console.log(addExpenses.split(','));
+console.log('budgetDay: ', budgetDay);
